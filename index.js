92d2500f7002def7a5c4486db3c7a143ffc56998
Prashant Verma <prashantvermaiiitb@gmail.com>
2022-06-17 16:01:10 +0530

    code update for dependent package

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log('this is an example of the scoped pacakge.');
-console.log('name mathlib is already taken we have used it.');
+module.exports = {
+    hello: function () {
+        console.log('this is an example of the scoped pacakge.');
+        console.log('name mathlib is already taken we have used it.');
+    }
+}
+
